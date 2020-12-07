a6e89a13f107212a838cb04f15c08f651b954b82
Jason Youngberg <jyoun8228@westada.org>
2020-12-07 15:05:32 -0700

    rework canvas.js

--- public/canvas.js
+++ public/canvas.js
@@ -1,28 +1,34 @@
+const grid_size = 10
+
 class Canvas {
 	constructor(ele){
+		this.ele = ele;
 		this.ctx = ele.getContext("2d")
+		this.ctx.lineWidth = grid_size
 	}
 
-	draw_circle(fill,x,y,r){
-		this.ctx.fillStyle = fill;
+	clear(){
+		ele.width  = window.innerWidth;
+		ele.height = window.innerHeight;
+		this.ctx.clearRect(0,0,ele.width,ele.height);
+	}
+
+	draw_circle(color,x,y){
+		this.ctx.fillStyle = color;
 		this.ctx.beginPath();
-		this.ctx.arc(x,y,r,0,2*Math.PI)
+		this.ctx.arc(x,y,grid_size/2,0,2*Math.PI)
 		this.ctx.fill();
 	}
 
-	draw_rect(fill,x0,y0,y1,y1){
-		this.ctx.fillStyle = fill;
-		this.ctx.fillRect(x0,y0,x1-x0,y1-y0);
+	draw_line(color,x0,y0,x1,y1){
+		this.ctx.strokeStyle = color;
+		this.ctx.beginPath();
+		this.ctx.moveTo(x0,y0);
+		this.ctx.lineTo(x1,y1);
+
 	}
 
-	draw_segment(color,x,y,start,end){
-		/*
-			0:Up
-			1:Right
-			2:Down
-			3:Left
-		*/
+	draw_px(color,x,y){
 
-		//TODO
 	}
 }
