cbf6f6dc51986c22f5b0f62cbfe226a7d1196dd1
Jason Youngberg <jyoun8228@westada.org>
2020-12-07 12:47:53 -0700

    hue settings

---
+++ public/index.js
@@ -0,0 +1,16 @@
+var ele = {//All elements shorthand
+	menu:document.getElementsByClassName('menu')[0],
+	color_slider:document.getElementById('color')
+}
+
+//Setup hue slider
+var hue = Math.floor(Math.random()*360);
+ele.menu.style.borderColor = `hsl(${hue},100%,50%)`;
+ele.menu.style.backgroundColor = `hsla(${hue},100%,50%,50%)`;
+ele.color_slider.value = hue;
+ele.color_slider.addEventListener('input',ev=>{
+	console.log(hue)
+	hue = ev.target.value;
+	ele.menu.style.borderColor = `hsl(${hue},100%,50%)`;
+	ele.menu.style.backgroundColor = `hsla(${hue},100%,50%,50%)`;
+})
