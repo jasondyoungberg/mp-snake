9d4701fc0704f471a95b5e1fe1a68367e9653d76
Jason Youngberg <jyoun8228@westada.org>
2020-12-07 12:47:26 -0700

    Reformat index.js

--- server/index.js
+++ server/index.js
@@ -4,31 +4,32 @@ const fs = require('fs');
 const express = require('express');
 const dateformat = require('dateformat');
 const config = JSON.parse(fs.readFileSync('config.json'));
-{//create http server
+
+//create http server
 const app = express();
 app.use((req,res,next)=>{//log request
-	log(`http://${req.hostname}${req.url}`,'http');
+	log(`${req.url}`,'http');
 	next();
 },express.static('public'));
 app.enable('etag');//enable caching
 const server = http.createServer(app);
-}
-{//create websocket server
+
+//create websocket server
 const wss = new ws.Server({noServer:true});
 server.on('upgrade',(req,soc,head)=>{
 	wss.handleUpgrade(req,soc,head,ws=>{
 		wss.emit('connection',ws,req);
 	})
 });
-}
+
 server.listen(config.port);//enable server
-{//setup date formatting
+
+//setup date formatting
 dateformat.masks.default = 'UTC:dd-mm-yyyy HH:MM:ss'
 function log(msg,type){//log message
 	var text = `[${dateformat(new Date())}] ${type}: ${msg}`;
 	if(config.log.console[type])console.log(text);
 	if(config.log.file[type])fs.appendFile(config.log.file.path,text+'\n',()=>{});
-}
 }
 
 var clients = []
