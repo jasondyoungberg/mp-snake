d225874bcbbd2cf800504ae7b90395067a2a28b5
Jason Youngberg <jyoun8228@westada.org>
2020-12-07 15:23:19 -0700

    add canvas and game loop

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
 var ele = {//All elements shorthand
 	menu:document.getElementsByClassName('menu')[0],
-	color_slider:document.getElementById('color')
+	color_slider:document.getElementById('color'),
+	game:document.getElementById('game'),
+	minimap:document.getElementById('minimap')
 }
 
 //Setup hue slider
@@ -13,3 +15,25 @@ ele.color_slider.addEventListener('input',ev=>{
 	ele.menu.style.borderColor = `hsl(${hue},100%,50%)`;
 	ele.menu.style.backgroundColor = `hsla(${hue},100%,50%,50%)`;
 })
+
+//Setup statistics monitoring
+stats = new Stats();
+stats.addPanel(new Stats.Panel('','#0000','#0000'));
+stats.showPanel(3);
+document.body.appendChild(stats.domElement);
+
+var canvas = {
+	game:new Canvas(ele.game),
+	minimap:new Canvas(ele.minimap)
+}
+
+var running = false
+
+function loop(){//Main game loop
+	stats.begin();
+	canvas.game.clear();
+	canvas.minimap.clear();
+	if(running){}
+	stats.end();
+	requestAnimationFrame(loop);
+}requestAnimationFrame(loop);
