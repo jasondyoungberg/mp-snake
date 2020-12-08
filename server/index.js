a170a1396552ebb197568cdb2cd54000f67ff0b3
Jason Youngberg <jyoun8228@westada.org>
2020-12-08 13:20:14 -0700

    Temporary echo for ws

--- server/index.js
+++ server/index.js
@@ -40,18 +40,14 @@ function log(msg,type){//log message
 
 var clients = []
 wss.on('connection',con=>{//handle websocket
-	function send(msg,type,data){
-		con.send(JSON.stringify({
-			id:msg.id,
-			time:Date.now(),
-			type:type,
-			data:data
-		}))
+	function send(msg){
+		con.send(JSON.stringify(msg));
 	}
 	con.on('message',data=>{
 		log(data,'ws')
 		try{
 			var msg = JSON.parse(data);
+			send(msg)
 			//TODO: handle message
 		}catch(err){
 			log(err,'err');
