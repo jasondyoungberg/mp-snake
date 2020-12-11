e014a853300587e4bfd76c1081c63a63e2fa4391
Jason Youngberg <jyoun8228@westada.org>
2020-12-11 10:19:59 -0700

    fix draw_grid()

--- public/game_canvas.js
+++ public/game_canvas.js
@@ -27,8 +27,8 @@ class GameCanvas {
 
 	draw_grid(){
 		this.ctx.strokeStyle = "#cccccc";
-		var target=camera.y+(this.h/grid_size);
-		for(var i=camera.x-(this.w/grid_size);i<=camera.x+(this.w/grid_size);i++){
+		var target=camera.y+Math.floor(this.h/grid_size)+0.5;
+		for(var i=camera.x-Math.floor(this.w/grid_size)-0.5;i<=target;i++){
 			var cord = this.cord.x(i);
 			this.ctx.beginPath();
 			this.ctx.moveTo(cord,0);
@@ -36,8 +36,8 @@ class GameCanvas {
 			this.ctx.stroke();
 		}
 
-		var target=camera.y+(this.h/grid_size);
-		for(var i=camera.y-(this.h/grid_size);i<=target;i++){
+		var target=camera.y+Math.floor(this.h/grid_size)+0.5;
+		for(var i=camera.y-Math.floor(this.h/grid_size)-0.5;i<=target;i++){
 			var cord = this.cord.y(i);
 			this.ctx.beginPath();
 			this.ctx.moveTo(0,cord);
